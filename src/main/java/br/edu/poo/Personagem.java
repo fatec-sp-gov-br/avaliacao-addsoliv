6328ffef5da8ed68b53c318ca94322c975848307
Andressa <addsoliv@gmail.com>
2026-06-17 21:22:34 -0300

    AndressaPersonagem.java

--- src/main/java/br/edu/poo/Personagem.java
+++ src/main/java/br/edu/poo/Personagem.java
@@ -2,34 +2,52 @@ package br.edu.poo;
 
 public class Personagem {
 
-    public Personagem(String nome,
-                      int vida,
-                      int ataque,
-                      int defesa) {
+    private String nome;
+    private int vida;
+    private int ataque;
+    private int defesa;
+    private boolean pocaoUtilizada;
 
+    public Personagem(String nome, int vida, int ataque, int defesa) {
+        this.nome = nome;
+        this.vida = vida;
+        this.ataque = ataque;
+        this.defesa = defesa;
+        this.pocaoUtilizada = false; 
     }
 
     public void atacar(Personagem inimigo) {
-
+        inimigo.receberDano(this.ataque);
     }
 
     public void receberDano(int dano) {
+        int danoReal = dano - this.defesa;
 
+        if (danoReal > 0) {
+            this.vida -= danoReal;
+        }
+
+        if (this.vida < 0) {
+            this.vida = 0;
+        }
     }
 
     public void usarPocao(Pocao pocao) {
-
+        if (!this.pocaoUtilizada) {
+            this.vida += pocao.getCura(); 
+            this.pocaoUtilizada = true;
+        }
     }
 
     public boolean estaVivo() {
-        return false;
+        return this.vida > 0;
     }
 
     public String getNome() {
-        return null;
+        return this.nome;
     }
 
     public int getVida() {
-        return 0;
+        return this.vida;
     }
 }
