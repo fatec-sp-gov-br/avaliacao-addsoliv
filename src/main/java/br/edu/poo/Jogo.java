c4ae2729e9863385af0413900f6d0878c8ad8537
Andressa <addsoliv@gmail.com>
2026-06-17 21:49:19 -0300

    Update Jogo.java

--- src/main/java/br/edu/poo/Jogo.java
+++ src/main/java/br/edu/poo/Jogo.java
@@ -3,9 +3,49 @@ package br.edu.poo;
 public class Jogo {
 
     public static void main(String[] args) {
+        Personagem artemis = new Personagem("Artemis", 30, 10, 2);
+        Personagem goblin = new Personagem("Goblin", 20, 5, 4);
+        Pocao pocaoPequena = new Pocao("Poção Pequena", 10);
 
-        // Implemente a batalha aqui
+        System.out.println("=== BATALHA INICIADA ===");
+        System.out.println();
 
+        boolean goblinUsouPocao = false; 
+
+        while (artemis.estaVivo() && goblin.estaVivo()) {
+
+            System.out.println(artemis.getNome() + " ataca " + goblin.getNome());
+            artemis.atacar(goblin);
+            System.out.println(goblin.getNome() + " possui " + goblin.getVida() + " de vida");
+            System.out.println();
+
+            if (!goblin.estaVivo()) {
+                break;
+            }
+
+            if (goblin.getVida() <= 14 && !goblinUsouPocao) {
+                System.out.println(goblin.getNome() + " usa " + pocaoPequena.getNome());
+                goblin.usarPocao(pocaoPequena);
+                goblinUsouPocao = true; 
+                System.out.println(goblin.getNome() + " possui " + goblin.getVida() + " de vida");
+                System.out.println();
+            } else {
+                System.out.println(goblin.getNome() + " ataca " + artemis.getNome());
+                goblin.atacar(artemis);
+                System.out.println(artemis.getNome() + " possui " + artemis.getVida() + " de vida");
+                System.out.println();
+            }
+        }
+
+        System.out.println("...");
+        System.out.println();
+        System.out.println("=== FIM DA BATALHA ===");
+        System.out.println();
+
+        if (artemis.estaVivo()) {
+            System.out.println("Vencedor: " + artemis.getNome());
+        } else {
+            System.out.println("Vencedor: " + goblin.getNome());
+        }
     }
-
 }
